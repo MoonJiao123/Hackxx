ab36cf522729d900acb057e0440451b74e403485
yitianwang18 <yiw031@ucsd.edu>
2019-04-06 18:36:35 -0700

    add an empty method

--- VoicedControlledCar/app/src/main/java/com/example/yuejiao/voicedcontrolledcar/MainActivity.java
+++ VoicedControlledCar/app/src/main/java/com/example/yuejiao/voicedcontrolledcar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yuejiao.voicedcontrolledcar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    /**
+     * sense the button press
+     */
+    public void enable_Voice(Button vButton){
 
     }
 }
